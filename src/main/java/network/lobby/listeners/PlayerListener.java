6a041c078e7092797d4d1cde3b73cb362d2300fd
Dylan <me@delxhq.me>
2022-07-12 21:03:04 +0100

    Cancel events in case the client somehow ignores our gamemode change.

--- src/main/java/network/lobby/listeners/PlayerListener.java
+++ src/main/java/network/lobby/listeners/PlayerListener.java
@@ -5,6 +5,10 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerListener implements Listener {
@@ -21,5 +25,22 @@ public class PlayerListener implements Listener {
 
         player.setGameMode(GameMode.ADVENTURE);
         player.sendTitle("Welcome!", null);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        event.setCancelled(true);
+    }
+    
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            event.setCancelled(true);
+        }
     }
 }
